df0d5b1ae62ab79b0350b85ce22702dd4f58db56
joshwhieb <joshwhieb@gmail.com>
2016-03-27 12:35:02 -0600

    Added run process as admin and wrapper script for running diskspd.

--- cdiskspd/cdiskspd.Tests.ps1
+++ cdiskspd/cdiskspd.Tests.ps1
@@ -2,8 +2,15 @@
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\$sut"
 
-Describe "cdiskspd" {
-    It "does something useful" {
-        $true | Should Be $false
+
+Describe "cdiskspd"{
+    It "Returns exit code 3. Function throws exception for nonzero exit code."{
+        Mock Run-ProcessAsAdministrator { "output, read, write", "3" }
+        {Run-Diskspd -arguments @()} | Should throw
+    }
+    It "Returns exit code 0. Correct call to diskspd and function runs without error."{
+        Mock run-ProcessAsAdministrator {"output, read, write", "0"}
+        $output, $error = Run-Diskspd -arguments @("-d10","c1G")
+        $error | Should be "0"
     }
 }
--- cdiskspd/cdiskspd.ps1
+++ cdiskspd/cdiskspd.ps1
@@ -1,3 +1,36 @@
-﻿function cdiskspd {
-
+﻿function Run-ProcessAsAdministrator($path, $arguments)
+{
+    $psi = New-object System.Diagnostics.ProcessStartInfo 
+    $psi.CreateNoWindow = $true 
+    $psi.UseShellExecute = $false 
+    $psi.RedirectStandardOutput = $true 
+    $psi.RedirectStandardError = $true 
+    $psi.FileName = $path
+    $psi.Arguments = $arguments 
+    $process = New-Object System.Diagnostics.Process 
+    $process.StartInfo = $psi 
+    $process.StartInfo.Verb = "RunAs"
+    $process.Start() | Out-Null
+    $output = $process.StandardOutput.ReadToEnd() 
+    $process.WaitForExit()
+    $process.Dispose()
+    $exitCode = $process.ExitCode 
+    return $output,$exitCode
 }
+
+function Run-Diskspd($path = "C:\Program Files\Diskspd\amd64fre\diskspd.exe", $arguments)
+{
+    $output, $exitCode = run-ProcessAsAdministrator -path $path -arguments $arguments
+
+    if($exitCode -ne 0)
+    {
+        throw "Nonzero exit code returned from diskspd"
+    }
+    else
+    {
+        return $output,$exitCode
+    }
+}
+
+
+## $output, $exitcode = run-ProcessAsAdministrator -path "C:\program files\Diskspd\amd64fre\diskspd.exe" -arguments @("-c1G","-b4K","-t2","-d10", "-a0,1", "testfile1.dat", "testfile2.dat")
